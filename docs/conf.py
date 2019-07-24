be1945ab2b8a809a41a19cc0a7c3ebc027b25e43
Tim Gates <tim.gates@iress.com>
2019-07-24 10:43:03 +1000

    Add autodoc extension

--- docs/conf.py
+++ docs/conf.py
@@ -57,6 +57,7 @@ release = read_version()
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
