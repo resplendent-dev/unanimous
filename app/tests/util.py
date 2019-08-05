1adfa7eef81a191e6b218de78094111cb77f7715
Tim Gates <tim.gates@iress.com>
2019-08-05 19:55:15 +1000

    Fix linting

--- app/tests/util.py
+++ app/tests/util.py
@@ -2,11 +2,12 @@
 import contextlib
 import os
 import shutil
+import sys
 import unittest
 import warnings
 from textwrap import dedent
+
 from pyspelling import spellcheck
-import sys
 
 # Below is general helper stuff that Python uses in `unittests`.  As these
 # not meant for users, and could change without notice, include them
@@ -29,8 +30,13 @@ def which(executable):
         if os.path.isfile(executable):
             location = executable
     else:
-        paths = [x for x in os.environ["PATH"].split(os.pathsep) if not x.isspace()]
-        paths.extend([x for x in os.environ.get("TOX_SPELL_PATH", "").split(os.pathsep) if not x.isspace()])
+        paths = [
+            x for x in os.environ["PATH"].split(os.pathsep) if not x.isspace()
+        ]
+        paths.extend([
+            x for x in os.environ.get("TOX_SPELL_PATH", "").split(os.pathsep)
+            if not x.isspace()
+        ])
         for path in paths:
             exe = os.path.join(path, executable)
             if os.path.isfile(exe):
@@ -68,11 +74,13 @@ def change_cwd(path, quiet=False):
 class PluginTestCase(unittest.TestCase):
     """Test plugin."""
 
-    def dedent(self, content):
+    @staticmethod
+    def dedent(content):
         """
         Normalize indentation of text.
 
-        When normalizing indentation of text, you often leave a new line at the start, so strip it as well.
+        When normalizing indentation of text, you often leave a new line at
+        the start, so strip it as well.
         """
 
         return dedent(content).replace('\n', '', 1)
@@ -81,17 +89,17 @@ class PluginTestCase(unittest.TestCase):
         """Make temp directory."""
 
         filename = os.path.join(self.tempdir, os.path.normpath(filename))
-        base, file = os.path.split(filename)
+        base = os.path.dirname(filename)
         if not os.path.exists(base):
             retry = 3
             while retry:
                 try:
                     os.makedirs(base)
                     retry = 0
-                except Exception:
+                except (IOError, OSError):
                     retry -= 1
-        with open(filename, "wb") as f:
-            f.write(content.encode(encoding))
+        with open(filename, "wb") as fobj:
+            fobj.write(content.encode(encoding))
 
     def setUp(self):
         """Setup."""
@@ -110,22 +118,27 @@ class PluginTestCase(unittest.TestCase):
             try:
                 shutil.rmtree(self.tempdir)
                 retry = 0
-            except Exception:
+            except (IOError, OSError):
                 retry -= 1
 
-    def assert_spell_required(self, running):
+    @staticmethod
+    def assert_spell_required(running):
         """Check if what we are running matches what we request."""
 
         required = os.environ.get("TOX_SPELL_REQUIRE", "any")
 
         if required not in ("both", "any", "aspell", "hunspell"):
-            raise RuntimeError("Invalid value of '{}' in 'TOX_SPELL_REQUIRE'".format(required))
+            raise RuntimeError(
+                "Invalid value of '{}' in 'TOX_SPELL_REQUIRE'".format(required)
+            )
 
         if running not in ("both", "aspell", "hunspell"):
             raise RuntimeError(
-                "Required tests are not being run (currently running '{}'). ".format(running) +
+                "Required tests are not being run"
+                " (currently running '{}'). ".format(running) +
                 "Make sure spell checker can be found and " +
-                "'TOX_SPELL_REQUIRE' is set appropriately (currently '{}')".format(required)
+                "'TOX_SPELL_REQUIRE' is set appropriately"
+                " (currently '{}')".format(required)
             )
 
         if required != running and (required != "any" and running != "both"):
@@ -134,9 +147,12 @@ class PluginTestCase(unittest.TestCase):
                 "'{}', is not compatible with '{}'".format(running, required)
             )
 
-    def assert_spellcheck(self, config_file, expected, names=None, groups=None, sources=None, verbose=4):
+    def assert_spellcheck(self, config_file, expected, verbose=4):
         """Spell check."""
 
+        names = None
+        groups = None
+        sources = None
         hunspell_location = which(HUNSPELL)
         aspell_location = which(ASPELL)
         if hunspell_location and aspell_location:
@@ -152,15 +168,14 @@ class PluginTestCase(unittest.TestCase):
         if hunspell_location:
             words = set()
             for results in spellcheck(
-                os.path.join(self.tempdir, config_file),
-                names=names,
-                groups=groups,
-                sources=sources,
-                checker='hunspell',
-                binary=hunspell_location,
-                debug=True,
-                verbose=verbose
-            ):
+                    os.path.join(self.tempdir, config_file),
+                    names=names,
+                    groups=groups,
+                    sources=sources,
+                    checker='hunspell',
+                    binary=hunspell_location,
+                    debug=True,
+                    verbose=verbose):
                 if results.error:
                     print(results.error)
                 words |= set(results.words)
@@ -168,15 +183,14 @@ class PluginTestCase(unittest.TestCase):
         if aspell_location:
             words = set()
             for results in spellcheck(
-                os.path.join(self.tempdir, config_file),
-                names=names,
-                groups=groups,
-                sources=sources,
-                checker='aspell',
-                binary=aspell_location,
-                debug=True,
-                verbose=verbose
-            ):
+                    os.path.join(self.tempdir, config_file),
+                    names=names,
+                    groups=groups,
+                    sources=sources,
+                    checker='aspell',
+                    binary=aspell_location,
+                    debug=True,
+                    verbose=verbose):
                 if results.error:
                     print(results.error)
                 words |= set(results.words)
