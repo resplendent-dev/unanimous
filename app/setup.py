e89edf726b107fe6bb0ab1715fecb5e216e07922
Tim Gates <tim.gates@iress.com>
2019-08-16 07:52:29 +1000

    Format with black

--- app/setup.py
+++ app/setup.py
@@ -12,29 +12,25 @@ import re
 # External Imports
 from setuptools import find_packages, setup
 
-PACKAGE_NAME = 'unanimous'
+PACKAGE_NAME = "unanimous"
+URL = "https://github.com/resplendent-dev/unanimous"
+GITHUB_ORG = "resplendent-dev"
+GITHUB_REPO = "unanimous"
+RE_SUB = "(https://github.com/%s/%s/blob/master/\\g<1>)" % (GITHUB_ORG, GITHUB_REPO)
 
 
-def load_readme(fname):
+def load_include(fname, transform=False):
     """
     Read the contents of relative `README` file.
     """
     file_path = os.path.join(os.path.dirname(__file__), fname)
-    with codecs.open(file_path, encoding='utf-8') as fobj:
-        sub = (
-            '(https://github.com/'
-            'resplendent-dev/unanimous'
-            '/blob/master/\\g<1>)'
-        )
-        markdown_fixed = re.sub(
-            '[(]([^)]*[.](?:md|rst))[)]',
-            sub,
-            fobj.read(),
-        )
+    with codecs.open(file_path, encoding="utf-8") as fobj:
+        data = fobj.read()
+        if not transform:
+            return data
+        markdown_fixed = re.sub("[(]([^)]*[.](?:md|rst))[)]", RE_SUB, data)
         rst_fixed = re.sub(
-            '^[.][.] [_][`][^`]*[`][:] ([^)]*[.](?:md|rst))',
-            sub,
-            markdown_fixed
+            "^[.][.] [_][`][^`]*[`][:] ([^)]*[.](?:md|rst))", RE_SUB, markdown_fixed
         )
         return rst_fixed
 
@@ -43,50 +39,50 @@ def read_version():
     """
     Read the contents of relative file.
     """
-    file_path = os.path.join(
-        os.path.dirname(__file__), PACKAGE_NAME, 'version.py'
-    )
-    regex = re.compile('__version__ = [\'\"]([^\'\"]*)[\'\"]')
-    with codecs.open(file_path, encoding='utf-8') as fobj:
+    file_path = os.path.join(os.path.dirname(__file__), PACKAGE_NAME, "version.py")
+    regex = re.compile("__version__ = ['\"]([^'\"]*)['\"]")
+    with codecs.open(file_path, encoding="utf-8") as fobj:
         for line in fobj:
             mobj = regex.match(line)
             if mobj:
                 return mobj.group(1)
-    raise Exception('Failed to read version')
+    raise Exception("Failed to read version")
 
 
 setup(
     name=PACKAGE_NAME,
     version=read_version(),
-    author='Tim Gates',
-    author_email='tim.gates@iress.com',
-    maintainer='Tim Gates',
-    maintainer_email='tim.gates@iress.com',
-    packages=find_packages(exclude=['tests']),
-    license='GPLv3+',
-    description=(
-        'Extension to make running pyspelling in CI easier. Maintains a common word exception list of technical terms so that every project does not need to exclude words like PyPi and GitHub etc..'
-    ),
-    long_description=load_readme('README.md'),
-    long_description_content_type='text/markdown',
+    author="Tim Gates",
+    author_email="tim.gates@iress.com",
+    maintainer="Tim Gates",
+    maintainer_email="tim.gates@iress.com",
+    packages=find_packages(exclude=["tests"]),
+    license="GPLv3+",
+    description=load_include("short_description.txt"),
+    long_description=load_include("README.md", transform=True),
+    long_description_content_type="text/markdown",
     python_requires=">=3.4",
     install_requires=[
-        elem for elem in
-        'pyspelling'.split('\n')
+        elem.strip()
+        for elem in load_include("requirements.txt").splitlines()
+        if elem.strip()
+    ],
+    url=URL,
+    classifiers=[
+        elem
+        for elem in [
+            "Development Status :: 4 - Beta",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: Implementation :: CPython",
+            "Programming Language :: Python :: Implementation :: PyPy",
+            "Operating System :: OS Independent",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        ]
         if elem
     ],
-    url='https://github.com/resplendent-dev/unanimous',
-    classifiers=[elem for elem in [
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-    ] if elem],
 )
