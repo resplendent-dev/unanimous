a2912d4b8e9896b8230878ac7cd4fe9455b543c0
Tim Gates <tim.gates@iress.com>
2019-12-31 21:56:57 +1100

    Add coverage

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,12 +1,15 @@
 """
 Test modules for unanimous.__main__
 """
+
+import pytest
 from click.testing import CliRunner
 
 from unanimous.__main__ import main
 
 
-def test_main():
+@pytest.mark.parametrize("args,expected", [([], 0), (["invoke"], 0)])
+def test_main(args, expected):
     """
     GIVEN the unanimous.__main__ module entry point WHEN calling main THEN the call
     executes successfully.
@@ -14,6 +17,6 @@ def test_main():
     # Setup
     runner = CliRunner()
     # Exercise
-    result = runner.invoke(main, [])
+    result = runner.invoke(main, args)
     # Verify
-    assert result.exit_code == 0  # nosec # noqa=S101
+    assert result.exit_code == expected  # nosec # noqa=S101
