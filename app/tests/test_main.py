4d812ce57a3335495d81f5aee409e34b09656556
Tim Gates <tim.gates@iress.com>
2020-01-28 11:03:36 +1100

    Remove non-word

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -37,5 +37,5 @@ def test_main(args, expected):
     new_words = load_master_zip()
     assert result.exit_code == expected  # nosec # noqa=S101
     assert len(original_words - new_words) == 0  # nosec # noqa=S101
-    # Allow for up to 5 new pypi packages to appear
+    # Allow for up to 5 new packages to appear
     assert len(new_words - original_words) < 5  # nosec # noqa=S101
