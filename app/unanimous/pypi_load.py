f75c199ea1d3fe5d5aae17e24f375b4fe1410d82
Tim Gates <tim.gates@iress.com>
2020-02-01 06:33:56 +1100

    Add escaping

--- app/unanimous/pypi_load.py
+++ app/unanimous/pypi_load.py
@@ -1,5 +1,5 @@
 """
-Load the list of packages from PyPi
+Load the list of packages from `PyPi`.
 """
 
 from defusedxml.xmlrpc import monkey_patch, unmonkey_patch, xmlrpc_client
@@ -7,7 +7,7 @@ from defusedxml.xmlrpc import monkey_patch, unmonkey_patch, xmlrpc_client
 
 def get_package_list():
     """
-    Load the list of packages from PyPi
+    Load the list of packages from `PyPi`.
     """
     monkey_patch()
     client = xmlrpc_client.ServerProxy("https://pypi.python.org/pypi")
