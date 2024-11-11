79abd1d28be77a7bfd92f88bac4e9be9faabd7bb
Tim Gates <tim.gates@iress.com>
2024-11-11 12:50:07 +1100

    version bump;

--- ci/shared/_docker_helper.sh
+++ ci/shared/_docker_helper.sh
@@ -2,8 +2,9 @@
 
 function docker_compose_run() {
     USEROPT="$(id -u):$(id -g)"
-    docker-compose -p "py${PYVER}" build --build-arg PYVER="${PYVER}"
-    docker-compose -p "py${PYVER}" up -d
-    docker-compose -p "py${PYVER}" run --rm -u "${USEROPT}" "$@"
-    docker-compose -p "py${PYVER}" down
+    SHORTPYVER="$(echo "${PYVER}" | sed "s/[.]//")"
+    docker-compose -p "py${SHORTPYVER}" build --build-arg PYVER="${PYVER}"
+    docker-compose -p "py${SHORTPYVER}" up -d
+    docker-compose -p "py${SHORTPYVER}" run --rm -u "${USEROPT}" "$@"
+    docker-compose -p "py${SHORTPYVER}" down
 }
